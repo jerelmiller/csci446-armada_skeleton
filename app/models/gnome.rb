a878f4eb36e71ff6aa0d9c469f4a643e03a849ac
Jerel Miller <jermille@mines.edu>
2011-04-21 11:32:30 -0600

    All but favorites

--- app/models/gnome.rb
+++ app/models/gnome.rb
@@ -9,7 +9,15 @@ class Gnome < ActiveRecord::Base
   has_many :favoritizations
   has_many :users, :through => :favoritizations
   
-
+  has_attached_file :photo,
+                      :styles => {
+                        :thumb => ["72x72#"],
+                        :medium => ["300x300#"]
+                      },
+                      :default_url => '/images/default_shield.png',
+                      :storage => :s3,
+                      :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                      :path => "cs446/mw_assoc/#{Rails.env}/:attachment/:id/:style.:extension"
   
   def self.most_recent
     first(:order => 'created_at DESC')
