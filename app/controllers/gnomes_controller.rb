779ab671ef7c05f7c207ddb0b409998cbc9a76b1
Jerel Miller <jermille@mines.edu>
2011-04-23 22:19:05 -0600

    fixing bug issues

--- app/controllers/gnomes_controller.rb
+++ app/controllers/gnomes_controller.rb
@@ -13,7 +13,11 @@ class GnomesController < ApplicationController
 
   def new
     if current_user
-      
+      if current_user.role.name =="member"
+        redirect_to new_member_gnome_path
+      elsif current_user.role.name == "admin" || current_user.role.name == "developer"
+        redirect_to new_admin_gnome_path
+      end
     else
       redirect_to login_path
     end
