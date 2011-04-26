3b7b31212d0c657e19c1818d3428d89a509dad34
Jerel Miller <jermille@mines.edu>
2011-04-26 17:16:11 -0600

    more link functionality added

--- app/helpers/gnomes_helper.rb
+++ app/helpers/gnomes_helper.rb
@@ -1,2 +1,20 @@
 module GnomesHelper
+  
+  def more_link(h)
+  	if h.description.length > 65
+  		link_to_function("more", nil, :id => "1") do |page|
+  			page["desc#{h.id}"].replace_html "#{h.description}"
+  		end
+  		
+  		less_link(h)
+  	end
+  end
+  
+  def less_link(h)
+    link_to_function("less", nil, :id => "1") do |page|
+      page["desc#{h.id}"].replace_html "#{truncate(h.description, :length => 65)}"
+    end
+    more_link(h)
+  end
+  
 end
