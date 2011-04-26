ec7553d112be61f5226e461cb15105586ef9d74b
Jerel Miller <jermille@mines.edu>
2011-04-26 15:17:41 -0600

    photo stuff

--- app/models/gnome.rb
+++ app/models/gnome.rb
@@ -12,15 +12,17 @@ class Gnome < ActiveRecord::Base
   cattr_reader :per_page
   @@per_page = 10
   
+  attr_accessor :photo_file_name
+  
   has_attached_file :photo,
-                      :styles => {
-                        :thumb => ["72x72#"],
-                        :medium => ["300x300#"]
-                      },
-                      :default_url => '/images/default_shield.png',
-                      :storage => :s3,
-                      :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-                      :path => "cs446/mw_assoc/#{Rails.env}/:attachment/:id/:style.:extension"
+                    :styles => {
+                      :thumb => ["72x72#"],
+                      :medium => ["300x300#"]
+                    },
+                    :default_url => '/images/default_shield.png',
+                    :storage => :s3,
+                    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                    :path => "cs446/mw_assoc/#{Rails.env}/:attachment/:id/:style.:extension"
   
   def self.most_recent
     first(:order => 'created_at DESC')
