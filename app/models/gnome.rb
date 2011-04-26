37f92a33130cde71846e774cb42a10333aa308d6
Jerel Miller <jermille@mines.edu>
2011-04-26 15:07:46 -0600

    photos stuff

--- app/models/gnome.rb
+++ app/models/gnome.rb
@@ -10,10 +10,7 @@ class Gnome < ActiveRecord::Base
   has_many :users, :through => :favoritizations
   
   has_attached_file :photo,
-                      :styles => {
-                        :thumb => ["72x72#"],
-                        :medium => ["300x300#"]
-                      },
+                      
                       :default_url => '/images/default_shield.png',
                       :storage => :s3,
                       :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
